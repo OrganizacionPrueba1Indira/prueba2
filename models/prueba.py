688a203d0c60478329beec6de015fc930ee0b0d1
Indira <idoconnor@uo.edu.cu>
2022-01-12 22:40:22 -0500

    @@

---
+++ models/prueba.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo import tools, _
+from odoo.exceptions import ValidationError
+
+class prueba(models.Model):
+    _name ='prueba'
+    _description = "prueba"
+    _order = "name"
+
+    name=fields.Char('CI')
+    permitido=fields.Boolean('Cuentas Permitidas')
+
